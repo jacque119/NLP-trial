e0cf315d9943dee5b9d7b030efe427e303cf48d4
jacque119 <rezaagassi9@gmail.com>
2025-04-08 10:03:59 +0700

    pusssshhh

--- deployment/app.py
+++ deployment/app.py
@@ -4,12 +4,12 @@ from eda import show_eda
 
 st.set_page_config(page_title="IMDB Sentiment Analyzer", layout="centered")
 
-st.title("IMDB Movie Review Sentiment Analyzer")
+st.title("IMDB Movie Reviews Sentiment Analyzer")
 
 menu = st.sidebar.selectbox("Menu", ["Predict Sentiment", "EDA"])
 
 if menu == "Predict Sentiment":
-    st.subheader("Enter a Movie Review:")
+    st.subheader("Enter a Movie Reviews:")
     user_input = st.text_area("Write your review here...")
 
     if st.button("Predict"):
